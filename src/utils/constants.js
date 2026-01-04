893617bc6c5c258ea4a40801ad5d9611972a0b35
kasasa22 <kasasatrevor25@gmail.com>
2026-01-04 19:33:26 +0300

    feat(utils): add constants file for centralized configuration

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const LOW_STOCK_THRESHOLD = 10
+
+export const ITEMS_PER_PAGE = 10
+
+export const TOAST_DURATION = 5000
+
+export const Z_INDEX = {
+  toast: 9999,
+  modal: 9999,
+  dropdown: 50,
+  sidebar: 40,
+  overlay: 30
+}
